1b164b4cd9360a0f74f2a0bb7d0d3886a94421b2
sonbk1997 <sonmsn1997@gmail.com>
2023-09-01 17:25:52 +0700

    update

--- expensive/src/main/java/com/management/expensive/ExpensiveManagementBeApplication.java
+++ expensive/src/main/java/com/management/expensive/ExpensiveManagementBeApplication.java
@@ -2,11 +2,21 @@ package com.management.expensive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class ExpensiveManagementBeApplication {
 
+	private static ApplicationContext applicationContext;
+
 	public static void main(String[] args) {
-		SpringApplication.run(ExpensiveManagementBeApplication.class, args);
+		applicationContext = SpringApplication.run(ExpensiveManagementBeApplication.class, args);
+		displayAllBeans();
+	}
+	public static void displayAllBeans() {
+		String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+		for(String beanName : allBeanNames) {
+			System.out.println(beanName);
+		}
 	}
 }
